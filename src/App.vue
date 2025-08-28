ef1ef2134f838fbe80376bdbc4e82d453ca363b6
Leonardo Ramos <leomatisa1000@gmai.com>
2025-08-28 12:30:11 -0400

    fix: renderização da vue principal

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
 import TheHeader from '@/components/domain/TheHeader.vue'
-import HomePage from './views/HomePage.vue'
 </script>
 
 <template>
   <div class="min-h-screen bg-neutral-100 font-sans text-neutral-800">
     <TheHeader />
-    <HomePage />
     <RouterView />
   </div>
 </template>
